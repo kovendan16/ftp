3ecf485fc1ba8dc2ed3a1fc5ca18d8cfb77557bb
kovendan16 <kovendan16@gmail.com>
2023-05-03 10:29:57 +0530

    multer validation added

--- models/File.js
+++ models/File.js
@@ -1,20 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const File = new mongoose.Schema({
-  path: {
-    type: String,
-    required: true,
+const File = new mongoose.Schema(
+  {
+    path: {
+      type: String,
+      required: true,
+    },
+    originalName: {
+      type: String,
+      required: true,
+    },
+    password: String,
+    downloadCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
   },
-  originalName: {
-    type: String,
-    required: true,
-  },
-  password: String,
-  downloadCount: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-})
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("File", File)
+module.exports = mongoose.model("File", File);
--- server.js
+++ server.js
@@ -1,22 +1,58 @@
 require("dotenv").config();
-
 const multer = require("multer");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const File = require("./models/File");
-const port = 4000;
+const path = require("path");
+const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const upload = multer({ dest: "uploads" });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    //some work
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.originalname.replace(/\.[^/.]+$/, "") +
+        "_" +
+        Date.now() +
+        path.extname(file.originalname)
+    );
+  },
+});
+
+let maxSize = 1024 * 1024 * 5;
+
+let upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: maxSize,
+  },
+  fileFilter: function (req, file, cb) {
+    console.log(file.mimetype);
+    let filetypes = /jpeg|jpg|png|gif|pdf|zip|docx|doc/;
+    let mimetype = filetypes.test(file.mimetype);
+    let extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+
+    cb(
+      "Error: File upload only supports the following filetypes: " + filetypes
+    );
+  },
+}).single("file");
 
 mongoose.set("strictQuery", false);
 
 mongoose.connect(
   process.env.DATABASE_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
-
   (err) => {
     if (!err) {
       console.log("db connected");
@@ -31,19 +67,31 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.post("/upload", async (req, res, next) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      if (err instanceof multer.MulterError && err.code == "LIMIT_FILE_SIZE") {
+        return res.send("File size is maximum 2mb");
+      }
 
-app.post("/upload", upload.single("file"), async (req, res) => {
-  const fileData = new File({
-    path: req.file.path,
-    originalName: req.file.originalname,
+      res.send(err);
+    } else {
+      const fileData = new File({
+        path: req.file.path,
+        originalName: req.file.originalname,
+      });
+
+      if (req.body.password != null && req.body.password !== "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10);
+      }
+
+      const file = await fileData.save();
+
+      res.render("index", {
+        fileLink: `${req.headers.origin}/file/${file.id}`,
+      });
+    }
   });
-  if (req.body.password != null && req.body.password !== "") {
-    fileData.password = await bcrypt.hash(req.body.password, 10);
-  }
-
-  const file = await fileData.save();
-
-  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
 app.route("/file/:id").get(handleDownload).post(handleDownload);
@@ -67,9 +115,16 @@ async function handleDownload(req, res) {
 
   console.log(file.downloadCount);
 
-  res.download(file.path, file.originalName);
+  res.download(file.path, file.originalName, (err) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .send({ message: "An error occurred while downloading the file." });
+    }
+  });
 }
 
 app.listen(port, (req, res) => {
-  console.log(`port working on  ${port}`);
+  console.log(`server started on port ${port}`);
 });
